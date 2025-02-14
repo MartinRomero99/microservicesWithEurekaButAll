73792bfeac9897e3c7420dd847b62eba982956c3
Martín Alejandro Romero <115097491+MartinRomero99@users.noreply.github.com>
2025-02-14 16:00:38 -0300

    Delete users-service directory

--- users-service/src/main/java/com/microsrv/users_service/service/IUserService.java
+++
@@ -1,8 +0,0 @@
-package com.microsrv.users_service.service;
-
-
-import com.microsrv.users_service.dto.UserDTO;
-
-public interface IUserService {
-    public UserDTO getUserAndPosts(Long user_id);
-}
